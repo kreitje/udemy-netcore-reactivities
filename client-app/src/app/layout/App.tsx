515b1ce9771504bcdc1137ac319bae6772e04265
Jeff Kreitner <kreitje@gmail.com>
2020-02-04 11:31:04 -0500

    End of section 5
    Create / Edit / Delete activities (locally no XHR)

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -12,6 +12,7 @@ const App = () => {
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter(a => a.id === id)[0]);
+    setEditMode(false);
   }
 
   const handleOpenCreateForm = () => {
@@ -19,10 +20,31 @@ const App = () => {
     setEditMode(true);
   }
 
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)]);
+  }
+
   useEffect(() => {
     axios.get<IActivity[]>('http://localhost:5000/api/activities')
       .then((response) => {
-        setActivities(response.data);
+        let activities : IActivity[] = [];
+        response.data.forEach(activity => {
+          activity.date = activity.date.split('.')[0]
+          activities.push(activity);
+        })
+        setActivities(activities);
       })
   }, []);
 
@@ -37,6 +59,9 @@ const App = () => {
           editMode={editMode}
           setEditMode={setEditMode}
           setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
         />
       </Container>
     </Fragment>
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,9 +5,10 @@ import { IActivity } from '../../../app/models/activity'
 interface IProps {
     activities: IActivity[],
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export const ActivityList : React.FC<IProps> = ({ activities, selectActivity }) => {
+export const ActivityList : React.FC<IProps> = ({ activities, selectActivity, deleteActivity }) => {
     return (
         <Segment clearing>
             <Item.Group divided>
@@ -26,6 +27,12 @@ export const ActivityList : React.FC<IProps> = ({ activities, selectActivity }) 
                                     content='View' 
                                     color='blue' 
                                     onClick={() => selectActivity(activity.id)}
+                                />
+                                <Button 
+                                    floated='right' 
+                                    content='Delete' 
+                                    color='red' 
+                                    onClick={() => deleteActivity(activity.id)}
                                 />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
